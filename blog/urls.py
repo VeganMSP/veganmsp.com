fbac7fd78d09dcfebc44909a94d70b4237abdce9
Matthew Jorgensen <matthew@jrgnsn.net>
2021-05-29 13:52:09 -0500

    Implement a simple blog app

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
+	path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, ListView
+
+from .models import Post
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+	queryset = Post.objects.filter(status=1).order_by('-date_created')
 	template_name = 'blog/index.html'
+
+
+class PostDetail(DetailView):
+	model = Post
+	template_name = 'blog/post_detail.html'

ab22728641483040503e879906cd8b0aa96e6780
Matthew Jorgensen <matthew@jrgnsn.net>
2021-05-29 18:41:16 -0500

    Implement CRUD views
    - Add PostModelForm and use it
    - Add template post_form.html (create/update)
    - Add template post_delete.html (confirms delete)
    - Update blog urls
    - Add edit/delete links to post_detail (visible if logged in)
    - Update index view with post dates
    - Sorted imports

---
+++ blog/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm, Textarea
+
+from .models import Post
+
+
+class PostModelForm(ModelForm):
+	class Meta:
+		model = Post
+		fields = (
+			'title',
+			'content',
+			'status',
+		)
+		widgets = {
+			'content': Textarea(
+				attrs={
+					'rows': 30,
+					'cols': 84,
+					'style': 'font-family: monospace'
+				})}
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+from datetime import datetime
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
 from django.utils.text import slugify
 
 POST_STATUS = (

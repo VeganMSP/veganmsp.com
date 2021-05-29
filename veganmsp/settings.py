df4cef2aa8a9adc6fa3246fa52ab98f476e14d0a
Matthew Jorgensen <matthew@jrgnsn.net>
2021-05-29 08:32:44 -0500

    Implement postgresql

--- veganmsp/settings.py
+++ veganmsp/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'veganmsp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

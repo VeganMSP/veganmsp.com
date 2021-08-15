1294dc87fe6ab07c64f7052647f947bad924013b
Matthew Jorgensen <matthew@jrgnsn.net>
2021-08-15 10:04:10 -0500

    Add trailing slashes to blog URLs

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ app_name = 'blog'
 urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
 	path('_c/', views.post_create, name='post_create'),
-	path('_u/<slug:slug>', views.post_update, name='post_update'),
-	path('_d/<slug:slug>', views.post_delete, name='post_delete'),
+	path('_u/<slug:slug>/', views.post_update, name='post_update'),
+	path('_d/<slug:slug>/', views.post_delete, name='post_delete'),
 	path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 
 	path(

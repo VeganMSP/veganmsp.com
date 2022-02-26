f534cc6998a262d8205f52082c42f9107c6a14f6
Matthew Jorgensen <matthew@jrgnsn.net>
2022-02-26 11:52:08 -0600

    Add login/logout actions, links, and templates

--- veganmsp/settings.py
+++ veganmsp/settings.py
@@ -136,6 +136,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Redirect Urls
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
 # Logging
 
 LOGGING = {
--- veganmsp/urls.py
+++ veganmsp/urls.py
@@ -17,9 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
     path('', include('info.urls')),
-
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
 ]

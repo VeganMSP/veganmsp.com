162692005ccd42a9349cd8cf8f31cd22dc4132b6
Matthew Jorgensen <matthew@jrgnsn.net>
2022-02-27 16:03:16 -0600

    Add email settings to example settings.py

--- veganmsp/secrets.example.py
+++ veganmsp/secrets.example.py
@@ -5,3 +5,15 @@ DATABASE_USER = 'django'
 DATABASE_PASS = ''
 DATABASE_HOST = '127.0.0.1'
 DATABASE_PORT = '5432'
+
+# SMTP Settings
+#
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_HOST_USER = 'django@example.com'
+EMAIL_HOST_PASSWORD = 'hunter2'
+EMAIL_PORT = 587
+EMAIL_SUBJECT_PREFIX = '[Django] '
+# Enable if you use TLS (usually port 587)
+# EMAIL_USE_TLS=True
+# Enable the following if you use SSL (usually port 465)
+# EMAIL_USE_SSL=True

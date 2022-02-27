f817fe7b8390d9fc533e1ec5884b2ea3c619b0a0
Matthew Jorgensen <matthew@jrgnsn.net>
2022-02-27 09:01:28 -0600

    Linted

--- blog/test_views.py
+++ blog/test_views.py
@@ -3,239 +3,241 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 from blog.models import (
-	Category,
-	Post,
+    Category,
+    Post,
 )
 
 
 class IndexViewTest(TestCase):
-	def test_view_url_exists_at_desired_location(self):
-		response = self.client.get('/')
-		self.assertEqual(response.status_code, 200)
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
 
-	def test_view_uses_correct_template(self):
-		response = self.client.get(reverse('blog:index'))
-		self.assertEqual(response.status_code, 200)
-		self.assertTemplateUsed(response, 'blog/index.html')
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('blog:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/index.html')
 
 
 class CategoryDetailViewTest(TestCase):
-	@classmethod
-	def setUpTestData(cls):
-		# Create category object
-		category = Category.objects.create(
-			name='Category'
-		)
-		cls.category = category
+    @classmethod
+    def setUpTestData(cls):
+        # Create category object
+        category = Category.objects.create(
+            name='Category'
+        )
+        cls.category = category
 
-	def test_view_url_exists_at_desired_location(self):
-		category = self.category
-		response = self.client.get(f'/blog/category/{category.id}/')
-		self.assertEqual(response.status_code, 200)
+    def test_view_url_exists_at_desired_location(self):
+        category = self.category
+        response = self.client.get(f'/blog/category/{category.id}/')
+        self.assertEqual(response.status_code, 200)
 
-	def test_view_uses_correct_template(self):
-		category = self.category
-		response = self.client.get(reverse('blog:category_detail', args=[category.id]))
-		self.assertEqual(response.status_code, 200)
-		self.assertTemplateUsed(response, 'blog/category_detail.html')
+    def test_view_uses_correct_template(self):
+        category = self.category
+        response = self.client.get(
+            reverse('blog:category_detail', args=[category.id])
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/category_detail.html')
 
 
 class PostCreateViewTest(TestCase):
-	@classmethod
-	def setUpTestData(cls):
-		# Create the user
-		user = User.objects.create_user(
-			username='test',
-			password='secret',
-		)
-		cls.user = user
+    @classmethod
+    def setUpTestData(cls):
+        # Create the user
+        user = User.objects.create_user(
+            username='test',
+            password='secret',
+        )
+        cls.user = user
 
-		# Create category object
-		category = Category.objects.create(
-			name='Category'
-		)
-		cls.category = category
+        # Create category object
+        category = Category.objects.create(
+            name='Category'
+        )
+        cls.category = category
 
-		authenticated_client = Client()
-		authenticated_client.login(username='test', password='secret')
-		cls.authenticated_client = authenticated_client
+        authenticated_client = Client()
+        authenticated_client.login(username='test', password='secret')
+        cls.authenticated_client = authenticated_client
 
-	def test_view_url_unauthenticated(self):
-		url = '/blog/_c/'
-		response = self.client.get(url)
-		self.assertEqual(response.status_code, 302)
+    def test_view_url_unauthenticated(self):
+        url = '/blog/_c/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
 
-	def test_view_url_exists_at_desired_location(self):
-		response = self.authenticated_client.get('/blog/_c/')
-		self.assertEqual(response.status_code, 200)
+    def test_view_url_exists_at_desired_location(self):
+        response = self.authenticated_client.get('/blog/_c/')
+        self.assertEqual(response.status_code, 200)
 
-	def test_view_url_accessible_by_name(self):
-		response = self.authenticated_client.get(reverse('blog:post_create'))
-		self.assertEqual(response.status_code, 200)
+    def test_view_url_accessible_by_name(self):
+        response = self.authenticated_client.get(reverse('blog:post_create'))
+        self.assertEqual(response.status_code, 200)
 
-	def test_view_uses_correct_template(self):
-		response = self.authenticated_client.get(reverse('blog:post_create'))
-		self.assertEqual(response.status_code, 200)
-		self.assertTemplateUsed(response, 'blog/post_form.html')
+    def test_view_uses_correct_template(self):
+        response = self.authenticated_client.get(reverse('blog:post_create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/post_form.html')
 
-	def test_valid_post(self):
-		response = self.authenticated_client.post(
-			reverse('blog:post_create'),
-			{
-				'title': 'My blog title',
-				'author': self.user,
-				'category': self.category.id,
-				'content': 'Post content!',
-				'status': 0
-			}
-		)
-		self.assertTrue(response.status_code, 200)
+    def test_valid_post(self):
+        response = self.authenticated_client.post(
+            reverse('blog:post_create'),
+            {
+                'title': 'My blog title',
+                'author': self.user,
+                'category': self.category.id,
+                'content': 'Post content!',
+                'status': 0
+            }
+        )
+        self.assertTrue(response.status_code, 200)
 
 
 class PostUpdateViewTest(TestCase):
-	@classmethod
-	def setUpTestData(cls):
-		# Create the user
-		user = User.objects.create_user(
-			username='test',
-			password='secret',
-		)
-		cls.user = user
+    @classmethod
+    def setUpTestData(cls):
+        # Create the user
+        user = User.objects.create_user(
+            username='test',
+            password='secret',
+        )
+        cls.user = user
 
-		# Create a category
-		category = Category.objects.create(
-			name='Category'
-		)
-		cls.category = category
+        # Create a category
+        category = Category.objects.create(
+            name='Category'
+        )
+        cls.category = category
 
-		# Create a post
-		post = Post.objects.create(
-			title='My blog title',
-			author=user,
-			category=category,
-			status=0,
-			content='My post content'
-		)
-		cls.post = post
+        # Create a post
+        post = Post.objects.create(
+            title='My blog title',
+            author=user,
+            category=category,
+            status=0,
+            content='My post content'
+        )
+        cls.post = post
 
-		authenticated_client = Client()
-		authenticated_client.login(username='test', password='secret')
-		cls.authenticated_client = authenticated_client
+        authenticated_client = Client()
+        authenticated_client.login(username='test', password='secret')
+        cls.authenticated_client = authenticated_client
 
-	def test_view_url_unauthenticated(self):
-		slug = self.post.slug
-		url = f'/blog/_u/{slug}/'
-		response = self.client.get(url)
-		self.assertTrue(response.status_code, 404)
+    def test_view_url_unauthenticated(self):
+        slug = self.post.slug
+        url = f'/blog/_u/{slug}/'
+        response = self.client.get(url)
+        self.assertTrue(response.status_code, 404)
 
-	def test_view_url_exists_at_desired_location(self):
-		slug = self.post.slug
-		response = self.authenticated_client.get(f'/blog/_u/{slug}/')
-		self.assertEqual(response.status_code, 200)
+    def test_view_url_exists_at_desired_location(self):
+        slug = self.post.slug
+        response = self.authenticated_client.get(f'/blog/_u/{slug}/')
+        self.assertEqual(response.status_code, 200)
 
-	def test_view_url_accessible_by_name(self):
-		slug = self.post.slug
-		response = self.authenticated_client.get(
-			reverse('blog:post_update', args=[slug])
-		)
-		self.assertEqual(response.status_code, 200)
+    def test_view_url_accessible_by_name(self):
+        slug = self.post.slug
+        response = self.authenticated_client.get(
+            reverse('blog:post_update', args=[slug])
+        )
+        self.assertEqual(response.status_code, 200)
 
-	def test_view_uses_correct_template(self):
-		slug = self.post.slug
-		response = self.authenticated_client.get(
-			reverse('blog:post_update', args=[slug])
-		)
-		self.assertEqual(response.status_code, 200)
-		self.assertTemplateUsed(response, 'blog/post_form.html')
+    def test_view_uses_correct_template(self):
+        slug = self.post.slug
+        response = self.authenticated_client.get(
+            reverse('blog:post_update', args=[slug])
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/post_form.html')
 
-	def test_valid_post(self):
-		post = self.post
-		slug = post.slug
+    def test_valid_post(self):
+        post = self.post
+        slug = post.slug
 
-		response = self.authenticated_client.post(
-			reverse('blog:post_update', args=[slug]),
-			{
-				'title': post.title,
-				'category': post.category_id,
-				'content': post.content,
-				'status': 1,
-			}
-		)
-		self.assertRedirects(
-			response,
-			expected_url=reverse('blog:post_detail', args=[slug]),
-			status_code=302,
-			target_status_code=200,
-			fetch_redirect_response=True
-		)
+        response = self.authenticated_client.post(
+            reverse('blog:post_update', args=[slug]),
+            {
+                'title': post.title,
+                'category': post.category_id,
+                'content': post.content,
+                'status': 1,
+            }
+        )
+        self.assertRedirects(
+            response,
+            expected_url=reverse('blog:post_detail', args=[slug]),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True
+        )
 
 
 class PostDeleteViewTest(TestCase):
-	@classmethod
-	def setUpTestData(cls):
-		# Create the user
-		user = User.objects.create_user(
-			username='test',
-			password='secret',
-		)
-		cls.user = user
+    @classmethod
+    def setUpTestData(cls):
+        # Create the user
+        user = User.objects.create_user(
+            username='test',
+            password='secret',
+        )
+        cls.user = user
 
-		# Create a category
-		category = Category.objects.create(
-			name='Category'
-		)
-		cls.category = category
+        # Create a category
+        category = Category.objects.create(
+            name='Category'
+        )
+        cls.category = category
 
-		# Create a post
-		post = Post.objects.create(
-			title='My blog title',
-			author=user,
-			category=category,
-			status=0,
-			content='My post content'
-		)
-		cls.post = post
+        # Create a post
+        post = Post.objects.create(
+            title='My blog title',
+            author=user,
+            category=category,
+            status=0,
+            content='My post content'
+        )
+        cls.post = post
 
-		authenticated_client = Client()
-		authenticated_client.login(username='test', password='secret')
-		cls.authenticated_client = authenticated_client
+        authenticated_client = Client()
+        authenticated_client.login(username='test', password='secret')
+        cls.authenticated_client = authenticated_client
 
-	def test_view_url_unauthenticated(self):
-		slug = self.post.slug
-		url = f'/blog/_d/{slug}/'
-		response = self.client.get(url)
-		self.assertTrue(response.status_code, 404)
+    def test_view_url_unauthenticated(self):
+        slug = self.post.slug
+        url = f'/blog/_d/{slug}/'
+        response = self.client.get(url)
+        self.assertTrue(response.status_code, 404)
 
-	def test_view_url_exists_at_desired_location(self):
-		slug = self.post.slug
-		response = self.authenticated_client.get(f'/blog/_d/{slug}/')
-		self.assertTrue(response.status_code, 200)
+    def test_view_url_exists_at_desired_location(self):
+        slug = self.post.slug
+        response = self.authenticated_client.get(f'/blog/_d/{slug}/')
+        self.assertTrue(response.status_code, 200)
 
-	def test_view_url_accessible_by_name(self):
-		slug = self.post.slug
-		response = self.authenticated_client.get(
-			reverse('blog:post_delete', args=[slug])
-		)
-		self.assertTrue(response.status_code, 200)
+    def test_view_url_accessible_by_name(self):
+        slug = self.post.slug
+        response = self.authenticated_client.get(
+            reverse('blog:post_delete', args=[slug])
+        )
+        self.assertTrue(response.status_code, 200)
 
-	def test_view_404_delete_nonexistent_item(self):
-		slug = self.post.slug + "nonsense"
-		response = self.authenticated_client.get(
-			reverse('blog:post_delete', args=[slug])
-		)
-		self.assertEqual(response.status_code, 404)
+    def test_view_404_delete_nonexistent_item(self):
+        slug = self.post.slug + "nonsense"
+        response = self.authenticated_client.get(
+            reverse('blog:post_delete', args=[slug])
+        )
+        self.assertEqual(response.status_code, 404)
 
-	def test_valid_post(self):
-		post = self.post
-		slug = post.slug
+    def test_valid_post(self):
+        post = self.post
+        slug = post.slug
 
-		response = self.authenticated_client.post(
-			reverse('blog:post_delete', args=[slug])
-		)
-		self.assertRedirects(
-			response,
-			expected_url=reverse('blog:index'),
-			status_code=302,
-			target_status_code=200,
-			fetch_redirect_response=True
-		)
+        response = self.authenticated_client.post(
+            reverse('blog:post_delete', args=[slug])
+        )
+        self.assertRedirects(
+            response,
+            expected_url=reverse('blog:index'),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True
+        )
--- veganmsp/default_admin_user.py
+++ veganmsp/default_admin_user.py
@@ -7,15 +7,15 @@ SU_PASS = 'admin'
 SU_MAIL = ''
 
 try:
-	superuser = User.objects.create_superuser(
-		username=SU_NAME,
-		password=SU_PASS,
-		email=SU_MAIL,
-		first_name='admin',
-		last_name='admin',
-	)
-	superuser.save()
+    superuser = User.objects.create_superuser(
+        username=SU_NAME,
+        password=SU_PASS,
+        email=SU_MAIL,
+        first_name='admin',
+        last_name='admin',
+    )
+    superuser.save()
 except IntegrityError:
-	print(f'Super user with {SU_NAME} already exists.')
+    print(f'Super user with {SU_NAME} already exists.')
 except Exception as e:
-	print(e)
+    print(e)

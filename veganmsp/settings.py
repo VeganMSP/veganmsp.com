6d7dd85b1b00af0f157ac81053b4975065ac63ef
Matthew Jorgensen <matthew@veganmsp.com>
2022-03-06 15:26:55 -0600

    Remove SECRET_KEY from settings.py

--- veganmsp/settings.py
+++ veganmsp/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = APP_SECRET  # noqa: F405
+#SECRET_KEY = APP_SECRET  # noqa: F405
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
